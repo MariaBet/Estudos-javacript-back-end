a86aa305f6d9c69eb46eb42147833ba745585e9f
MariaBet <mariabetania543.com>
2022-08-01 17:57:50 -0300

    exercicio

---
+++ exer1.js
@@ -0,0 +1,20 @@
+/* No JavaScript, assim como na maioria das linguagens de programação, é possível armazenar dados na memória do computador através de variáveis.
+
+Esses dados podem ser palavras, números ou valores lógicos como “verdadeiro” ou “falso” (chamados de tipos booleanos), como ilustra a tabela abaixo: */
+
+
+
+const idade = 2;
+const nome = "Lara";
+const sobrenome = "Ayumi";
+const bacharel = false;
+const dezAnosDepois = 12;
+
+const nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
+
+const resultado = idade + dezAnosDepois
+console.log(resultado);
+
+console.log(bacharel == 1);
