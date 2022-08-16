d0ac2ced241574e57180405e450947ce7ab6c825
MariaBet <mariabetania543.com>
2022-08-16 09:30:29 -0300

    operador ternanrio

--- exer1.js
+++ exer1.js
@@ -4,7 +4,7 @@ Esses dados podem ser palavras, números ou valores lógicos como “verdadeiro�
 
 
 
-const idade = 2;
+const idades = 2;
 const nome = "Lara";
 const sobrenome = "Ayumi";
 const bacharel = false;
@@ -14,7 +14,20 @@ const nomeCompleto = nome + sobrenome;
 
 console.log(nomeCompleto);
 
-const resultado = idade + dezAnosDepois
+const resultado = idades + dezAnosDepois
 console.log(resultado);
 
 console.log(bacharel == 1);
+
+
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+}
+
+console.log(respostaDeTudo, idade, pi)
---
+++ operadorternario.js
@@ -0,0 +1,15 @@
+
+
+const idadeMinina = 18;
+const idadeCliente = 16;
+
+if (idadeCliente >= idadeMinina) {
+    console.log("Cerveja")
+} else{
+    console.log("Suco")
+}
+
+
+
+
+console.log(idadeMinina >= idadeCliente ? "cerveja" : " suco");
