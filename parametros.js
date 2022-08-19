c7cfb9cdc2f2617c77b20721613f3ec1a9a68353
MariaBet <mariabetania543.com>
2022-08-19 16:17:12 -0300

    novos

---
+++ parametros.js
@@ -0,0 +1,34 @@
+
+
+function soma(numero1, numero2){
+    return numero1 + numero2;
+
+}
+
+console.log(soma(2, 2))
+console.log(soma(245, 20))
+console.log(soma(500, 53))
+
+function nomeIdade(nome, idade){
+    return `Meu nome é ${nome} e minha idade é ${idade}`;
+}
+
+console.log(nomeIdade('Fernanda', 32))
+
+function multiplica(numero1, numero2){
+    return numero1 * numero2;
+}
+
+
+console.log(multiplica(soma(4,5),soma(3, 3)))
+
+function comParametro(param) {
+    console.log(param)
+}
+comParametro()
+
+
+
+
+
+
